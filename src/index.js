d850b15db66fbe043b7c23bb6b5889187f9ec0d0
Martin <mr.mmaurer+work@gmail.com>
2019-09-03 23:45:31 +0100

    Added image loading

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('test');
+import fs from 'fs-extra';
+import { PNG } from 'pngjs';
+import { rejects } from 'assert';
+
+fs.readFile('./src/stage.png')
+  .then((data) => {
+    const { length: bytesLoaded } = data;
+    console.log(`${bytesLoaded} loaded`);
+
+    const parsedPNG = new Promise((resolve, reject) => {
+      new PNG({ filterType: 4 })
+        .parse(data, (error, dataParsed) => {
+          if (error) {
+            reject(error);
+          }
+          resolve(dataParsed);
+        });
+    });
+
+    parsedPNG
+      .then((dataParsed) => {
+        console.error(dataParsed);
+      })
+      .catch((error) => console.error(error));
+  })
+  .catch((error) => console.error(error));
