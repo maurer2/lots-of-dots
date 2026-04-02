e2d0a78ed52a5f891cfb1d349fc320cea7798c55
Martin <mr.mmaurer@gmail.com>
2026-04-02 23:29:46 +0200

    Added basic animation

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,20 @@
-<script>
+<script lang="ts">
   import Dots from "./lib/components/Dots/Dots.svelte";
   // import NumberStepper from "./lib/components/NumberStepper/NumberStepper.svelte";
 
   const defaultNumberOfDots = 5;
   let numberOfDots = $state(defaultNumberOfDots);
+
+  function changeNumber(newCount: typeof numberOfDots) {
+    numberOfDots += newCount;
+  }
+
+  function reset() {
+    numberOfDots = defaultNumberOfDots;
+  }
 </script>
 
-<main>
+<main class="main">
   <section aria-label="Dots">
     <Dots count={numberOfDots} />
   </section>
@@ -14,29 +22,45 @@
   <section>
     <h2 id="contact-heading">Controls</h2>
 
-    <div role="group" aria-label="Dots control">
-      <output for="button-plus-one button-plus-ten button-reset"
-        >{numberOfDots}</output
+    <div role="group" aria-label="Dots control" class="control-strip">
+      <output
+        for="button-plus-one button-plus-ten button-reset"
+        class="current-value-indicator">{numberOfDots}</output
       >
 
       <button
         type="button"
         id="button-plus-one"
-        aria-label="Increade count by 1"
-        onclick={() => (numberOfDots = numberOfDots + 1)}>+1</button
+        aria-label="Increase count by 1"
+        onclick={() => changeNumber(1)}>+1</button
       >
       <button
         type="button"
         id="button-plus-ten"
-        aria-label="Increade count by 10"
-        onclick={() => (numberOfDots = numberOfDots + 10)}>+10</button
+        aria-label="Increase count by 10"
+        onclick={() => changeNumber(10)}>+10</button
       >
       <button
         type="button"
         id="button-reset"
         aria-label="Reset count"
-        onclick={() => (numberOfDots = defaultNumberOfDots)}>Reset</button
+        onclick={() => reset()}>Reset</button
       >
     </div>
   </section>
 </main>
+
+<style>
+  @layer components {
+    .control-strip {
+      display: flex;
+      gap: 0.5rem;
+    }
+
+    .current-value-indicator {
+      padding-inline: 1rem;
+      align-content: center;
+      border: 1px solid currentColor;
+    }
+  }
+</style>
--- src/lib/components/Dots/Dots.svelte
+++ src/lib/components/Dots/Dots.svelte
@@ -6,13 +6,29 @@
   let { count }: DotsProps = $props();
 </script>
 
-<div role="status" aria-label="Loading">
-  {#each { length: count }, currentCount}
-    <span aria-hidden="true" class="dot">{currentCount}</span>
-  {/each}
-</div>
+{#key count}
+  <div role="status" aria-label="Loading" class="dots">
+    {#each { length: count }}
+      <span aria-hidden="true" class="dot"></span>
+    {/each}
+  </div>
+{/key}
 
 <style>
   @layer components {
+    .dots {
+      display: flex;
+      gap: 0.5rem;
+    }
+
+    .dot {
+      inline-size: 1rem;
+      aspect-ratio: 1;
+      border: 1px solid currentColor;
+      animation: opacity-fade-in 1s ease-in-out infinite;
+      animation-delay: calc((sibling-index()) * calc(1s / sibling-count()));
+      background: red;
+      flex: 0 0 auto;
+    }
   }
 </style>
